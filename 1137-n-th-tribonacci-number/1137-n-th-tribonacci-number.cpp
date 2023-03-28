dba075bb9756f56948f561d50ea0b89e2cc589e4
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-03-29 01:26:28 +0530

    Time: 0 ms (100.00%), Space: 6 MB (40.68%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int v[n+1];
+        
+        if(n<2){
+          return n;
+        }
+        if(n==2){
+            return 1;
+        }
+        v[0]=0;
+        v[1]=1;
+        v[2]=1;
+        for(int i=3;i<=n;i++){
+            v[i]=v[i-1]+v[i-2]+v[i-3];
+        }
+        return v[n];
+    }
+};
