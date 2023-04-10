0e8282e0f209a4fdc3a2c246fa044dcc06ed03e8
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-04-10 21:01:22 +0530

    Time: 4 ms (35.43%), Space: 6 MB (52.65%) - LeetHub

---
+++ 0780-reaching-points/0780-reaching-points.cpp
@@ -0,0 +1,45 @@
+class Solution
+{
+    public:
+        bool reachingPoints(int sx, int sy, int tx, int ty)
+        {
+           	// if(sx%sy==0){
+           	//      sx=sx;
+           	//      sy=sy;
+           	// } 
+           	// else{if(sx < sy){
+           	//          sy=sy%sx;
+           	//  }       
+           	//  else sx=sx%sy;}
+            int mini = min(sx, sy);
+            int maxi = max(sx, sy);
+            while (tx >= sx && ty >= sy)
+            {
+              if (tx < ty)
+                {
+                    if(sx==tx)
+                    {
+                            if((ty-sy)%tx==0)
+                            {
+                                    return true;
+                            }
+                            else return false;
+                    }
+                    ty = ty % tx;
+                }
+                else
+                {
+                  if(sy==ty){
+                            if((tx-sx)%ty==0)
+                            {
+                                    return true;
+                            }
+                           else return false;
+                    }
+                    tx = tx % ty;
+                }
+             
+            }
+            return false;
+        }
+};
