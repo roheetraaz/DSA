b682d7a423bd9158d98407055ed6325335c69c4a
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-04-10 12:19:43 +0530

    Time: 4 ms (39.51%), Space: 6.4 MB (12.09%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,37 +1,20 @@
-class Solution {
-public:
-    bool isValid(string s) {
-        stack<char> st;
-        unordered_map<char,char>mp;
-        mp[')']='(';
-        mp[']']='[';
-        mp['}']='{';
-        
-        for(auto i:s)  
+class Solution
+{
+    public:
+        bool isValid(string s)
         {
-            if(i=='(' || i=='{' || i=='[') st.push(i);
-            else
+            stack<char> st;
+            unordered_map<char, char> mp;
+            mp[')'] = '(';  mp[']'] = '[';  mp['}'] = '{';
+            for (auto i: s)
             {
-                if(st.empty() or (mp[i]!=st.top())) return false;
-                st.pop();
+                if (i == '(' || i == '{' || i == '[') st.push(i);
+                else
+                {
+                    if (st.empty() or(mp[i] != st.top())) return false;
+                    st.pop();
+                }
             }
+            return st.empty();
         }
-        return st.empty();
-    }
 };
-// class Solution {
-// public:
-//     bool isValid(string s) {
-//         stack<char> st;
-//         for(auto i:s)  
-//         {
-//             if(i=='(' || i=='{' || i=='[') st.push(i);
-//             else
-//             {
-//                 if(st.empty() or (st.top()=='(' and i!=')') or (st.top()=='{' and i!='}') or (st.top()=='[' and i!=']')) return false;
-//                 st.pop();
-//             }
-//         }
-//         return st.empty();
-//     }
-// };
