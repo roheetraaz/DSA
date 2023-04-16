2f1675e3f28bc161d6f2a7d5034d1fa51fa95970
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-04-16 10:08:20 +0530

    Time: 1251 ms (33.33%), Space: 31.8 MB (33.33%) - LeetHub

--- 2644-find-the-maximum-divisibility-score/2644-find-the-maximum-divisibility-score.cpp
+++ 2644-find-the-maximum-divisibility-score/2644-find-the-maximum-divisibility-score.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int maxDivScore(vector<int>& nums, vector<int>& divisors) {
-                
-            
-            
-            vector<pair<int,int>>ans;
-            for(int i=0;i<divisors.size();i++){
-                
+        int n=divisors.size();
+        int m =nums.size();
+        int maxxi=0;
+        sort(divisors.begin(),divisors.end());
+        if(n==1){
+                return divisors[0];
+        }
+        vector<pair<int,int>>ans;
+        for(int i=0;i<n;i++){
                 int count=0;
-                for(int j=0;j<nums.size();j++){
+                for(int j=0;j<m;j++)
+                {
                         if(nums[j]%divisors[i]==0){
                                 count++;
-                        }
-                  
+                        } 
                 }
-                // maxi=max(count,maxi);
-               
+                cout<<maxxi<<" "<<divisors[i]<<endl;
                 ans.push_back({count,divisors[i]});
         }
          sort(ans.begin(),ans.end());
@@ -28,6 +30,6 @@ public:
                          break;
                  }
          }
-           return final[0];
+         return final[0];
     }
 };
