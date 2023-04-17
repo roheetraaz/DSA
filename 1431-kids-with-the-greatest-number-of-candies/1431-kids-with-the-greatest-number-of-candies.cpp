d5034e4baaa302a262f03a0a71fb57ec69afc582
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-04-18 00:43:11 +0530

    Time: 3 ms (66.58%), Space: 8.8 MB (86.24%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -4,15 +4,11 @@ class Solution
         vector<bool> kidsWithCandies(vector<int> &candies, int e)
         {
             int z = *max_element(candies.begin(), candies.end());
-                cout<<z;
             vector<bool> ans;
             for (int i = 0; i < candies.size(); i++)
             {
-                if (candies[i]-z+e>=0){
-                        ans.push_back(true);
-                }
-                else ans.push_back(false);
+                ans.push_back(candies[i] - z + e >= 0);
             }
-        return ans;
+            return ans;
         }
 };
