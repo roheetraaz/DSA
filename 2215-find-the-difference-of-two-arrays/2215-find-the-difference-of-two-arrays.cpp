6bfd65318700367070965b767f89bbe0c27997ae
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-05-04 11:06:03 +0530

    Time: 49 ms (86.15%), Space: 31.1 MB (80.31%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -1,35 +1,14 @@
-class Solution
-{
-    public:
-        vector<vector < int>> findDifference(vector<int> &nums1, vector<int> &nums2)
-        {
-            vector<int> v;
-            vector<vector < int>> ans;
-            unordered_map<int, int> mp,mp2,mp3;
-            vector<int> a;
-            for (int i = 0; i < nums1.size(); i++)
-            {
-                for (int j = 0; j < nums2.size(); j++)
-                {
-                    if (nums1[i] ==nums2[j])
-                    {
-                        mp[nums1[i]]++;
-                    }
-                }
-            }
-            for (auto i: nums1)
-            {
-                if (mp.find(i) == mp.end()) mp2[i]++;
-            }
-            for (auto i: nums2)
-            {
-                if (mp.find(i) == mp.end()) mp3[i]++;
-            }
-            for(auto i :mp2)v.push_back(i.first);
-            for(auto i :mp3)a.push_back(i.first);
-                
-            ans.push_back(v);
-            ans.push_back(a);
-            return ans;
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& A, vector<int>& B) {
+        unordered_set<int> sa(begin(A), end(A)), sb(begin(B), end(B));
+        vector<vector<int>> ans(2);
+        for (int n : sa) {
+            if (sb.count(n) == 0) ans[0].push_back(n);
         }
+        for (int n : sb) {
+            if (sa.count(n) == 0) ans[1].push_back(n);
+        }
+        return ans;
+    }
 };
