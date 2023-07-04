a24dea5e453a2209cbf7b9fbdc987c4b403b0a08
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-07-04 19:52:44 +0530

    Time: 16 ms (11.56%), Space: 9.8 MB (34.34%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
        unordered_map<int,int>mp;
-        queue<int>q;
         for(auto i :nums){
                 mp[i]++;
                 if(mp[i]==3){
