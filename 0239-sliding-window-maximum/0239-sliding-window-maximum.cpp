8cffdd66786847805a92e9c3b228941376cd5fbe
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-08-16 21:34:47 +0530

    Time: 263 ms (77.74%), Space: 148.7 MB (28.1%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        set<pair<int,int>>s;
-        for(int i=0; i<k-1; i++)s.insert({nums[i],i});
-        vector<int>ans;
-        for(int i=k-1; i<nums.size(); i++){
-            s.insert({nums[i],i});
-
-            auto it  = prev(s.end());
-            ans.push_back(it->first);
-            s.erase({nums[i+1-k],i+1-k});
+        priority_queue<pair<int,int>> pq;
+        vector<int> ans;
+        for(int i=0;i<k-1;i++)
+        {
+            pq.push({nums[i],i});
+        }
+        for(int i=k-1;i<nums.size();i++)
+        {
+            pq.push({nums[i],i});
+            int y=pq.top().second;
+            int x=pq.top().first;
+            while(!(y<=i&&y>=i-k+1))
+            {
+                pq.pop();
+                y=pq.top().second;
+                x=pq.top().first;
+            }
+            ans.push_back(x);
         }
         return ans;
     }
