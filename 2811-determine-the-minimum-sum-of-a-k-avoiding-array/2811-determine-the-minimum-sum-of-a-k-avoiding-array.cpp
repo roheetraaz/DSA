82b0ab8a211f7c83c44895db0d5e0f00947f5e8b
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-08-20 10:42:54 +0530

    Time: 64 ms (20%), Space: 12.6 MB (20%) - LeetHub

--- 2811-determine-the-minimum-sum-of-a-k-avoiding-array/2811-determine-the-minimum-sum-of-a-k-avoiding-array.cpp
+++ 2811-determine-the-minimum-sum-of-a-k-avoiding-array/2811-determine-the-minimum-sum-of-a-k-avoiding-array.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     int minimumSum(int n, int k) {
-        unordered_map<int,int> mp;
-        int sum = 0;
-        int j = 0, num = 1;
-        while (j < n) {
-            int diff = k - num;
-            if (mp.find(diff) == mp.end()) {
-                mp[num]++;
-                sum += num;
-                num++;
-                j++;
-            } else {
-                num++;
+        set<int>ans;
+        vector<int>d1;
+        for(int i =1;i<=n;i++)d1.push_back(i);
+        int k1=1;
+        for(int i=0;i<d1.size();i++){
+            for(int j=0;j<d1.size();j++){
+                if(d1[i]+d1[j]==k && i!=j){
+                    ans.insert(max(d1[i],d1[j]));
+                    d1.push_back(n+k1);
+                    k1++;
+                }
             }
         }
+        for(auto i : ans)cout<<i<<" ";
+
+        vector<int>final;
+        int sum=0;
+        unordered_map<int,int>mp;
+        for(auto i :ans)mp[i]++;
+        int l=1;
+        while(final.size()!=n){
+            if(mp.find(l)==mp.end())final.push_back(l);
+            l++;
+        }
+        for(auto i : final)sum+=i;
         return sum;
     }
 };
