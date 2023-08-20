d9e4989a74c5e0eae9738064bb5a5811c53c1229
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-08-20 10:46:46 +0530

    Time: 43 ms (20%), Space: 10.5 MB (100%) - LeetHub

--- 2811-determine-the-minimum-sum-of-a-k-avoiding-array/2811-determine-the-minimum-sum-of-a-k-avoiding-array.cpp
+++ 2811-determine-the-minimum-sum-of-a-k-avoiding-array/2811-determine-the-minimum-sum-of-a-k-avoiding-array.cpp
@@ -1,31 +1,41 @@
-class Solution {
-public:
-    int minimumSum(int n, int k) {
-        set<int>ans;
-        vector<int>d1;
-        for(int i =1;i<=n;i++)d1.push_back(i);
-        int k1=1;
-        for(int i=0;i<d1.size();i++){
-            for(int j=0;j<d1.size();j++){
-                if(d1[i]+d1[j]==k && i!=j){
-                    ans.insert(max(d1[i],d1[j]));
-                    d1.push_back(n+k1);
-                    k1++;
+class Solution
+{
+    public:
+        int minimumSum(int n, int k)
+        {
+            vector<int> ans;
+            vector<int> d1;
+            for (int i = 1; i <= n; i++) d1.push_back(i);
+            int k1 = 1;
+            for (int i = 0; i < d1.size(); i++)
+            {
+                for (int j = 0; j < d1.size(); j++)
+                {
+                    if (d1[i] + d1[j] == k && i != j)
+                    {
+                        ans.push_back(max(d1[i], d1[j]));
+                        d1.push_back(n + k1);
+                        k1++;
+                    }
                 }
             }
-        }
-        for(auto i : ans)cout<<i<<" ";
 
-        vector<int>final;
-        int sum=0;
-        unordered_map<int,int>mp;
-        for(auto i :ans)mp[i]++;
-        int l=1;
-        while(final.size()!=n){
-            if(mp.find(l)==mp.end())final.push_back(l);
-            l++;
+            int sum = 0;
+            unordered_map<int, int> mp;
+            for (auto i: ans) mp[i]++;
+            int l = 1;
+            int siz = 0;
+            while (siz != n)
+            {
+                if (mp.find(l) == mp.end())
+                {
+                    sum += l;
+                    siz++;
+                }
+
+                l++;
+            }
+
+            return sum;
         }
-        for(auto i : final)sum+=i;
-        return sum;
-    }
 };
