a4daeb5c5438a3a9c23d269b223793577dd95219
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-08-24 13:17:51 +0530

    Time: 0 ms (100%), Space: 6.7 MB (18.73%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+    public:
+        int dp[1001];
+    int n;
+    vector<int>nums;
+
+    int solve(int target)
+
+    {
+
+        if (target < 0) return 0;
+
+        if (target == 0) return 1;
+          int ans = 0;
+        if (dp[target] != -1) return dp[target];
+      
+        for (int j = 0; j < n; j++)
+        {
+            ans = ans+solve(target - nums[j]);
+        }
+
+        dp[target] = ans;
+        return ans;
+    }
+    int combinationSum4(vector<int> &num, int target)
+    {
+        memset(dp, -1, sizeof(dp));
+        nums=num;
+        n = nums.size();
+        solve(target);
+        return dp[target];
+    }
+};
