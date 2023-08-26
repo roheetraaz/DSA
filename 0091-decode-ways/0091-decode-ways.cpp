f0de48d42b2b7f4b486f3dc7493c24fda94f7aed
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-08-26 22:06:11 +0530

    Time: 8 ms (8.36%), Space: 16.3 MB (5.68%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,23 +1,17 @@
 class Solution
 {
     public:
-    // int ans=0;
-    int dp[100100];
+        int dp[100100];
     int solve(int ind, string s)
-    {   
-        if(ind > s.size())return 0;
+    {
+        if (ind > s.size()) return 0;
         if (ind == s.size()) return 1;
+        if (dp[ind] != -1) return dp[ind];
+        int ans = 0;
 
-        if(dp[ind] != -1)return dp[ind];
-        int ans=0;
+        if (s[ind] == '0') return dp[ind] = 0;
 
-        if (s[ind] == '0')
-        {
-
-            return dp[ind]= 0;
-        }
-
-        else if (s[ind] == '1' or(s[ind] <= '2' && (ind + 1) < s.size() and s[ind+1]>='0' and s[ind + 1] <= '6'))
+        else if (s[ind] == '1' or (s[ind] == '2' && (ind + 1) < s.size() && s[ind + 1] <= '6'))
         {
             ans += solve(ind + 1, s) + solve(ind + 2, s);
         }
@@ -26,8 +20,7 @@ class Solution
     }
     int numDecodings(string s)
     {
-        memset(dp,-1,sizeof(dp));
+        memset(dp, -1, sizeof(dp));
         return solve(0, s);
-         
     }
 };
