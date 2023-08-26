878163678d7ae7ffacba3f1147859ac79adf6252
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-08-26 23:42:43 +0530

    Time: 10 ms (85.36%), Space: 10.5 MB (69.32%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution { // 8 ms, faster than 91.61%
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> sub;
+        for (int x : nums) {
+            if (sub.empty() || sub[sub.size() - 1] < x) {
+                sub.push_back(x);
+                cout<<x<<endl;
+            } else {
+                auto it = lower_bound(sub.begin(), sub.end(), x); // Find the index of the first element >= x
+                *it = x; // Replace that number with x
+            }
+        }
+        return sub.size();
+    }
+};
