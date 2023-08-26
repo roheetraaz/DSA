e787ddab15df0779aa0c1ccb9f6af61c45639c0a
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-08-26 14:41:58 +0530

    Time: 71 ms (79.18%), Space: 25.6 MB (34.23%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -7,11 +7,9 @@ class Solution
             if (dp[idx] != -1) return dp[idx];
 
             int notTake = solve(idx + 1, offers, dp);
-
-            vector<int> target = { offers[idx][1],
-                1001
-            };	// Use INT_MAX for upper_bound
-            int next_idx = upper_bound(offers.begin(), offers.end(), target) - offers.begin();
+            vector<int>v={offers[idx][1],1001 };
+           	// Use INT_MAX for upper_bound and Pass vector
+            int next_idx = upper_bound(offers.begin(), offers.end(),v)-offers.begin();
 
             int take = 1 + solve(next_idx, offers, dp);
 
