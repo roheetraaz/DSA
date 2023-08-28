11477a1180a42564a1c2dce014a2a18e48f0480d
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-08-28 19:13:26 +0530

    Time: 3 ms (82.27%), Space: 10.2 MB (6.6%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -1,13 +1,29 @@
-class Solution {
-public:
-    int findKthPositive(vector<int>& arr, int k) {
-    int lo = 0, hi = arr.size();
-    while (lo < hi) {
-        int mid = lo + (hi - lo) / 2;
-        if (arr[mid] - mid > k) hi = mid;
-        else lo = mid + 1;
+class Solution
+{
+    public:
+        vector<int> arr;
+    int k;
+    bool check(int mid)
+    {
+        return (arr[mid] - mid) > k;
     }
-    return lo + k;
-}
+    int findKthPositive(vector<int> &a, int l)
+    {
+        int lo = 0, hi = a.size()-1;
+        int ans = hi+1;
+        arr = a;
+        k = l;
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+            if (check(mid))
+            {
+                ans = mid;
+                hi = mid - 1;
+            }
+            else lo = mid + 1;
+        }
+        return ans + l;
+    }
 };
 // sexy soln
