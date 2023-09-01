155ab017d839e95860dc33351032bdb4de12810e
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-09-01 18:21:51 +0530

    Time: 65 ms (40.77%), Space: 57.9 MB (5%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -1,31 +1,32 @@
-class Solution {
-public:
-    int longestOnes(vector<int>& nums, int k) {
-    //    Sliding Window Approch
-        int j=0;
-        int max_con_one=0;
-        int count_zero=0;
-        // Traverse over the Array
-        for(int i=0;i<nums.size();i++)
+class Solution
+{
+    public:
+        int longestOnes(vector<int> &nums, int k)
         {
-            // if element is Zero -> Increase ZeroCount
-            if(nums[i]==0)
-            count_zero++;
+            int n = nums.size();
+            int i = 0;
+            int j = 0;
+            int len = 0;
+            vector<int> prefsumi(nums.size() + 1, 0);
+            int l = 0;
 
-            // if The Zero Count Exceed k(Max Zero) then Shrink Our Window Size
-            while(count_zero > k)
+            for (int i = 0; i <= nums.size(); i++)
             {
-                // if zero comes then decrease zero Count
-                if(nums[j]==0)
-                count_zero--;
-                // Update j to Shrink Window Size
+                prefsumi[i] = l;
+                if (i < n)
+                {
+                    l += nums[i];
+                }
+            }
+            while (i < n && j < n)
+            {
+                while (i <= j && j - i + 1 - (prefsumi[j + 1] - prefsumi[i]) > k)
+                {
+                    i++;
+                }
+                len = max(len, j - i + 1);
                 j++;
             }
-            // Update result in Every Step
-            int window_length=i-j+1;
-            // +1 is for 0 based Index
-            max_con_one=max(max_con_one,window_length);
-        } 
-        return max_con_one;
-    }
+            return len;
+        }
 };
