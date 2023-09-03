8085c3fb0552b043a4d094bd9c6a6588619b697d
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-09-04 01:22:22 +0530

    Time: 0 ms (100%), Space: 6.1 MB (58.51%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,13 +1,15 @@
-class Solution {
-public:
-    int integerBreak(int n) {
-        int ans=1;
-        if(n==2)return 1;
-        if(n==3)return 2;
-        while(n>4){
-            ans=ans*3;
-            n=n-3;
+class Solution
+{
+    public:
+        int integerBreak(int n)
+        {
+            int ans = 1;
+            if (n == 2) return 1;
+            if (n == 3) return 2;
+            ans = pow(3, (n / 3 - 1));
+            if (n % 3 == 1)ans = ans * 4;
+            else if (n % 3==2)ans=ans*6;
+            else ans=ans*3;
+            return ans;
         }
-        return ans*n;
-    }
 };
