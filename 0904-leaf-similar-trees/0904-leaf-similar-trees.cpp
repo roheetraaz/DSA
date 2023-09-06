919ee64291fdad08fd8f9583d8a088ebc1eaa5af
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-09-06 15:03:38 +0530

    Time: 0 ms (100%), Space: 12.7 MB (94.36%) - LeetHub

--- 0904-leaf-similar-trees/0904-leaf-similar-trees.cpp
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.cpp
@@ -14,23 +14,24 @@ class Solution
     public:
         void rec(TreeNode *tr, vector<int> &v1)
         {
+            if (tr == NULL) return;
             if (tr->left == nullptr && tr->right == nullptr)
             {
                 v1.push_back(tr->val);
                 return;
             }
-            
-            if (tr->left != nullptr) rec(tr->left, v1);
-            if (tr->right != nullptr) rec(tr->right, v1);
+
+            rec(tr->left, v1);
+            rec(tr->right, v1);
         }
     bool leafSimilar(TreeNode *root1, TreeNode *root2)
     {
         vector<int> v1, v2;
         rec(root1, v1);
         rec(root2, v2);
-        // for(auto i : v1)cout<<i<<" ";
-        // cout<<endl;
-        //  for(auto i : v2)cout<<i<<" ";
+       	// for(auto i : v1)cout<<i<<" ";
+       	// cout<<endl;
+       	//  for(auto i : v2)cout<<i<<" ";
         return v1 == v2;
     }
 };
