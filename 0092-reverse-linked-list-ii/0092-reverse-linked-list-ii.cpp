e68bc798216a3b14c581cb116545803171669caf
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-09-07 22:12:34 +0530

    Time: 0 ms (100%), Space: 7.4 MB (46.43%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -1,30 +1,52 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *int val;
+ *ListNode * next;
+ *ListNode() : val(0), next(nullptr) {}
+ *ListNode(int x) : val(x), next(nullptr) {}
+ *ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
-     vector<int>v;
-     ListNode* temp=new ListNode(0);
-     ListNode* temp2=temp;
-     while(head!=NULL){
-         v.push_back(head->val);
-         head=head->next;
-     }   
-     reverse(v.begin()+left-1,v.begin()+right);
-     for(auto i : v){
-         temp->next=new ListNode(i);
-         temp=temp->next;
-     }
-     
-     
-     return temp2->next;
-    }
+class Solution
+{
+    public:
+        ListNode* reverseBetween(ListNode *head, int left, int right)
+        {
+            ListNode *nexti = NULL;
+            ListNode *prev = NULL;
+            ListNode *ans = new ListNode(0);
+            ListNode * res=ans;
+            int x=left;
+            while (left - 1 && head!=NULL)
+            {
+                ans->next = new ListNode(head->val);
+                ans = ans->next;
+                head = head->next;
+
+                left--;
+            }
+       
+            right = right - x+1;
+            while (head != NULL && right >0)
+            {
+                nexti = head->next;
+                head->next = prev;
+                prev = head;
+                head = nexti;
+                right--;
+            }
+            ans->next = prev;
+            while (ans->next != NULL)
+            {
+                ans = ans->next;
+            }
+            while (head != NULL)
+            {
+                ans->next = new ListNode(head->val);
+                ans = ans->next;
+                head = head->next;
+            }
+            return res->next;
+        }
 };
