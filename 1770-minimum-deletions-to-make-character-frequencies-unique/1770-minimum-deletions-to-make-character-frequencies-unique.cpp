862d7e616d2e9cf369e630c0723340d42d734117
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-09-12 12:54:44 +0530

    Time: 81 ms (34.58%), Space: 17.4 MB (37.86%) - LeetHub

--- 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -8,7 +8,7 @@ class Solution
             for (auto i: mp1) mp2[i.second]++;
             int cnt = 0;
             int k = mp1.size();
-            while (mp2.size() != mp1.size())
+            while (mp2.size() != k)
             {
                 for (auto i: mp2)
                 {
@@ -16,11 +16,13 @@ class Solution
                     {
                         cnt++;
                         mp2[i.first]--;
+                        // decreasing the most frequent frequency by 1;
                         if (i.first != 1)
                         {
+                           	// increasing the frequeny of elements with freq_count 1 less by 1
                             mp2[i.first - 1]++;
-                        
                         }
+                       	// if the frequency of element is only one,then we will delete the element in map1
                         else k--;
                         if (mp2.size() == k) return cnt;
                     }
