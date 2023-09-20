065a95c9abcbcdd1cc1f19e8d27c8f8680bb8ff7
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-09-21 00:31:15 +0530

    Time: 598 ms (5.05%), Space: 247.2 MB (5.04%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -4,31 +4,46 @@ class Solution
         int minOperations(vector<int> &nums, int x)
         {
             int n = nums.size();
-            int sum = 0;
-            vector<int> s = nums;
-            for (auto i: nums) s.push_back(i);
-            nums = s;
-            int ans = INT_MAX;
-            int i = 0, j = 0, curr = 0;
-            while (j < nums.size())
+            vector<int> pl(n, 0);
+            vector<int> pr(n, 0);
+            pl[0] = nums[0];
+            for (int i = 1; i < n; i++)
             {
-                curr += nums[j];
-
-                while (curr > x)
+                pl[i] = pl[i - 1] + nums[i];
+            }
+            pr[0] = nums[n - 1];
+            for (int i = n - 2; i >= 0; i--)
+            {
+                pr[n - i - 1] = pr[n - i - 2] + nums[i];
+            }
+            int ans = INT_MAX;
+            unordered_map<int, int> mpl, mpr;
+            for (int i = 0; i < n; i++) mpl[pl[i]] = i + 1;
+            for (int i = 0; i < n; i++) mpr[pr[i]] = i + 1;
+            mpl[0] = 0;
+            mpr[0] = 0;
+            for (auto i: pl)
+            {
+                if (mpr.find(x - i) != mpr.end())
                 {
-                    curr -= nums[i];
-                    i++;
-                }
-                if (curr == x)
-                {
-                    if ((j < n - 1 && i == 0) || (j >= n - 1 && i >= j - n + 1 && i <= n))
+                    int z = mpr[x - i] + mpl[i];
+                    if (z <= n)
                     {
-                        ans = min(ans, j - i + 1);
+                        ans = min(ans, z);
                     }
                 }
-              
-                j++;
             }
-            return (ans == INT_MAX) ? -1 : ans;
+            for (auto i: pr)
+            {
+                if (mpl.find(x - i) != mpl.end())
+                {
+                    int z = mpl[x - i] + mpr[i];
+                    if (z <= n)
+                    {
+                        ans = min(ans, z);
+                    }
+                }
+            }
+            return ans == INT_MAX ? -1 : ans;
         }
 };
