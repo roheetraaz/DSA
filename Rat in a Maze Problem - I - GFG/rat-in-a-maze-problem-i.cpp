81305186a7b97d9cc4d39a62f1f14b8073e199be
roheetraaz <98976706+roheetraaz@users.noreply.github.com>
2023-11-15 10:57:07 +0530

    Added solution - LeetHub

---
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+
+class Solution {
+public:
+    vector<string> ans;
+    int n;
+    vector<vector<int>> grid;
+
+    bool isvalid(int i, int j) {
+        if (i >= 0 && i < n && j >= 0 && j < n && grid[i][j] == 1)
+            return true;
+        return false;
+    }
+
+    void solve(int i, int j, string &ds, vector<vector<int>> &vis) {
+        if (i == n - 1 && j == n - 1) {
+            ans.push_back(ds);
+            return;
+        }
+        if (isvalid(i + 1, j) && vis[i + 1][j] != 1) {
+            ds += 'D';
+            vis[i + 1][j] = 1;
+            solve(i + 1, j, ds, vis);
+            ds = ds.substr(0, ds.size() - 1);
+            vis[i + 1][j] = 0;
+        }
+        if (isvalid(i, j - 1) && vis[i][j - 1] != 1) {
+            ds += 'L';
+            vis[i][j - 1] = 1;
+            solve(i, j - 1, ds, vis);
+            ds = ds.substr(0, ds.size() - 1);
+            vis[i][j - 1] = 0;
+        }
+        if (isvalid(i, j + 1) && vis[i][j + 1] != 1) {
+            ds += 'R';
+            vis[i][j + 1] = 1;
+            solve(i, j + 1, ds, vis);
+            ds = ds.substr(0, ds.size() - 1);
+            vis[i][j + 1] = 0;
+        }
+        if (isvalid(i - 1, j) && vis[i - 1][j] != 1) {
+            ds += 'U';
+            vis[i - 1][j] = 1;
+            solve(i - 1, j, ds, vis);
+            ds = ds.substr(0, ds.size() - 1);
+            vis[i - 1][j] = 0;
+        }
+    }
+
+    vector<string> findPath(vector<vector<int>> &m, int k) {
+        grid = m;
+        n = k;
+        string ds;
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        vis[0][0]=1;
+        if(grid[0][0]!=1)return ans;
+        solve(0, 0, ds, vis);
+        
+        return ans;
+    }
+};
+
+
+    
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> m(n, vector<int> (n,0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> m[i][j];
+            }
+        }
+        Solution obj;
+        vector<string> result = obj.findPath(m, n);
+        sort(result.begin(), result.end());
+        if (result.size() == 0)
+            cout << -1;
+        else
+            for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
